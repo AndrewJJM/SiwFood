ad27706fd2fb00e58719bc48864753089f33f0e4
AndrewJJM <andjohnmurugan100@gmail.com>
2024-05-27 18:21:16 +0200

    Added basic SpringSecurity implementation

--- src/main/java/it/uniroma3/siw/repository/RecipeRepository.java
+++ src/main/java/it/uniroma3/siw/repository/RecipeRepository.java
@@ -3,7 +3,6 @@ package it.uniroma3.siw.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import it.uniroma3.siw.model.Cook;
 import it.uniroma3.siw.model.Recipe;
 
 /*Si interfaccia direttamente con il database per l'ENTITà SPECIFICA e ci fornisce operazioni CRUD (Create, Read, Update, Delete)
